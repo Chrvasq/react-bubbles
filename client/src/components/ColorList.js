cca14bc18708c690dcb48b57a22a0751975d8e91
Chris Vasquez <chrvasq@outlook.com>
2019-12-13 20:17:41 -0800

    Add functionality to add colors

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -6,10 +6,17 @@ const initialColor = {
   code: { hex: "" }
 };
 
+const initialErrors = {
+  color: "",
+  hex: ""
+};
+
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState(initialColor);
+  const [errors, setErrors] = useState(initialErrors);
 
   const editColor = color => {
     setEditing(true);
@@ -41,6 +48,32 @@ const ColorList = ({ colors, updateColors }) => {
       .then(() => {
         setUpdatedColors();
       });
+  };
+
+  const addColor = e => {
+    e.preventDefault();
+    if (newColor.color !== "" && newColor.code.hex !== "") {
+      axiosWithAuth()
+        .post("/colors", newColor)
+        .then(() => {
+          setUpdatedColors();
+          setNewColor(initialColor);
+          setErrors(initialErrors);
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    } else if (newColor.color === "" && newColor.code.hex === "") {
+      setErrors({
+        color: "You need to enter a color",
+        hex: "You need to enter a hex code"
+      });
+      console.log(errors);
+    } else if (newColor.color === "") {
+      setErrors({ color: "You need to enter a color" });
+    } else if (newColor.code.hex === "") {
+      setErrors({ hex: "You need to enter a hex code" });
+    }
   };
 
   return (
@@ -98,8 +131,29 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
+      <form onSubmit={addColor}>
+        <label>Color</label>
+        <input
+          value={newColor.color}
+          onChange={e =>
+            setNewColor({
+              ...newColor,
+              color: e.target.value
+            })
+          }
+        />
+        {errors.color && <p>{errors.color}</p>}
+        <label>Hex Code</label>
+        <input
+          value={newColor.code.hex}
+          onChange={e =>
+            setNewColor({ ...newColor, code: { hex: e.target.value } })
+          }
+        />
+        {errors.hex && <p>{errors.hex}</p>}
+        <button>Add Color</button>
+      </form>
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
     </div>
   );
 };
